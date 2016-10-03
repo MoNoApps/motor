8c016f04140a3b293d8892faa4ba09f7aa99785c
Daniel Martinez <dmartz86@gmail.com>
2016-10-03 12:44:21 -0500

    Fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -18,16 +18,16 @@ describe('App: Motor', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  it(`should have a title`, async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    expect(app.title).toEqual('Motor');
   }));
 
-  it('should render title in a h1 tag', async(() => {
+  it('should render title h1', async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('h1').textContent).toContain('Motor: Rules As A Service');
   }));
 });
